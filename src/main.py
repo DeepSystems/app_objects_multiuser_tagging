9c258cf171c3fb7cb7ab0b98185b9929720e287a
Max Kolomeychenko <max@supervise.ly>
2020-07-06 17:06:19 +0300

    next/prev object

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import sys
 import json
 import os
+import threading
+
 import supervisely_lib as sly
+from supervisely_lib.api.module_api import ApiField
 
 my_app = sly.AppService()
 
@@ -11,15 +14,94 @@ user2upc_local_path = os.path.join(my_app.app_dir, "user2upc_demo.json")
 
 user2selectedUpc = {}
 
+anns_lock = threading.Lock()
+anns = {}
+
+metas_lock = threading.Lock()
+metas = {}
+
 #@my_app.callback(sly.app.STOP_COMMAND)
 #def stop(api: sly.Api, task_id, context, state):
 #    sys.exit(0)
 
 
-# @my_app.callback("assign")
-# @sly.timeit
-# def assign(api: sly.Api, task_id, context, state):
-#     pass
+def get_annotation(api: sly.Api, project_id, image_id):
+    global anns
+    if image_id not in anns:
+        meta = get_project_meta(api, project_id)
+        ann_json = api.annotation.download(image_id).annotation
+        ann = sly.Annotation.from_json(ann_json, meta)
+
+        global anns_lock
+        anns_lock.acquire()
+        anns[image_id] = ann
+        anns_lock.release()
+
+    return anns[image_id]
+
+
+def get_project_meta(api: sly.Api, project_id):
+    global metas
+    if project_id not in metas:
+        meta_json = api.project.get_meta(project_id)
+        meta = sly.ProjectMeta.from_json(meta_json)
+
+        global metas_lock
+        metas_lock.acquire()
+        metas[project_id] = meta
+        metas_lock.release()
+
+    return metas[project_id]
+
+
+def get_first_id(ann: sly.Annotation):
+    return ann.labels[0].geometry.sly_id
+
+
+def get_prev_id(ann: sly.Annotation, active_figure_id):
+    for idx, label in enumerate(ann.labels):
+        if label.geometry.sly_id == active_figure_id:
+            if idx == 0:
+                return None
+            return ann.labels[idx - 1].geometry.sly_id
+
+
+def get_next_id(ann: sly.Annotation, active_figure_id):
+    for idx, label in enumerate(ann.labels):
+        if label.geometry.sly_id == active_figure_id:
+            if idx == len(ann.labels) - 1:
+                return None
+            return ann.labels[idx + 1].geometry.sly_id
+
+
+def select_object(api: sly.Api, context, find_func):
+    user_id = context["userId"]
+    image_id = context["imageId"]
+    project_id = context["projectId"]
+    ann_tool_session = context["sessionId"]
+
+    ann = get_annotation(api, project_id, image_id)
+
+    active_figure_id = context["figureId"]
+    if active_figure_id is None:
+        active_figure_id = get_first_id(ann)
+    else:
+        active_figure_id = find_func(ann, active_figure_id)
+
+    if active_figure_id is not None:
+        api.img_ann_tool.set_figure(ann_tool_session, active_figure_id)
+
+
+@my_app.callback("prev_object")
+@sly.timeit
+def prev_object(api: sly.Api, task_id, context, state):
+    select_object(api, context, get_prev_id)
+
+
+@my_app.callback("next_object")
+@sly.timeit
+def prev_object(api: sly.Api, task_id, context, state):
+    select_object(api, context, get_next_id)
 
 
 def main():
@@ -41,7 +123,8 @@ def main():
 
     data = {
         "user2upc": user2upc,
-        "user2selectedUpc": user2selectedUpc
+        "user2selectedUpc": user2selectedUpc,
+        "selectedObjectId": -1,
     }
 
     # state
